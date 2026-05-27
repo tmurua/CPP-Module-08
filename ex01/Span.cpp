d2cca44178952f8e2f1887edb89445b16eafb828
tmurua <tommurua@gmail.com>
2026-05-28 01:11:38 +0200

    feat: ex01: implement shortestSpan() and longestSpan()

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 16:33:07 by tmurua            #+#    #+#             */
-/*   Updated: 2026/05/27 17:36:17 by tmurua           ###   ########.fr       */
+/*   Updated: 2026/05/28 01:10:43 by tmurua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,33 @@ void Span::addNumber(int number){
 	if (_numbers.size() >= _maxSize)
 		throw std::runtime_error("Error: Span is already full");
 	_numbers.push_back(number);
+}
+
+// shortest span = smallest difference between 2 stored numbers
+int Span::shortestSpan() const{
+	if (_numbers.size() < 2)
+		throw std::runtime_error("Error: not enough numbers to find a span");
+
+	std::vector<int> sortedNumbers = _numbers;
+	std::sort(sortedNumbers.begin(), sortedNumbers.end());
+
+	int shortest = sortedNumbers[1] - sortedNumbers[0];
+
+	for (std::size_t i = 1; i < sortedNumbers.size() - 1; i++){
+		int currentSpan = sortedNumbers[i + 1] - sortedNumbers[i];
+		if (currentSpan < shortest)
+			shortest = currentSpan;
+	}
+	return (shortest);
+}
+
+// longest span = biggest difference between smallest and biggest number
+int Span::longestSpan() const{
+	if (_numbers.size() < 2)
+		throw std::runtime_error("Error: not enough numbers to find a span");
+
+	int smallest = *std::min_element(_numbers.begin(), _numbers.end());
+	int biggest = *std::max_element(_numbers.begin(), _numbers.end());
+
+	return (biggest - smallest);
 }
