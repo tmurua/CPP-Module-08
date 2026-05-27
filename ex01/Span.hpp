c955649adecca021929e96cee917649c7ea836d5
tmurua <tommurua@gmail.com>
2026-05-27 17:26:33 +0200

    feat: ex01: start implementation of Span class, declare orthodox canonical form, addNumber(), shortestSpan(), longestSpan()  and implement template function addNumbers

---
+++ ex01/Span.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 10:23:25 by tmurua            #+#    #+#             */
+/*   Updated: 2026/05/27 17:24:33 by tmurua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+#include <stdexcept>	// std::runtime_error
+
+class Span
+{
+private:
+	unsigned int		_maxSize;
+	std::vector<int>	_numbers;
+
+public:
+	Span();
+	Span(unsigned int maxSize);
+	Span(const Span &other);
+	Span &operator=(const Span &other);
+	~Span();
+
+	void addNumber(int number);
+
+	// add many numbers at once using a range of iterators
+	template <typename IteratorType>
+	void addNumbers(IteratorType begin, IteratorType end){
+		while (begin != end){
+			if (_numbers.size() >= _maxSize)
+				throw std::runtime_error("Error: Span is already full");
+			_numbers.push_back(*begin);
+			++begin;
+		}
+	}
+
+	int shortestSpan() const;
+	int longestSpan() const;
+};
+
+#endif
